0150cb5571db0088d997ebf198997d0baf7959f5
Bromor <158762685+Br0m0r@users.noreply.github.com>
2024-03-19 17:42:18 +0200

    New

---
+++ Math Functions/Math Functions/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace MathFunct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Find highest or lower value between 2 values
+            int lowerst = Math.Min(1, 2);
+            int highest = Math.Max(1, 2);
+
+            //Round Up or Down
+            //If its halfway between,it always rounds towards the even integer
+            double rounded = Math.Round(9.5); //10
+            double rounded2 = Math.Round(10.5); //10
+            double rounded3 = Math.Round(10.123123, 2); //Second argument specifies how many digits ro round e.g. here it gives 10.123
+            double rounded4 = Math.Round(-11.5, MidpointRounding.AwayFromZero); //11
+            //force a round up or down
+            double roundD = Math.Floor(9.9); // 9
+            double roundU = Math.Ceiling(9.1); // 10
+
+            //trigonometric functs
+            double sin = Math.Sin(0);
+            double cos = Math.Cos(0);
+            double tan = Math.Tan(0);
+            double arcSin = Math.Asin(0);
+            double arcCos = Math.Acos(0);
+            double arcTan = Math.Atan(0);
+
+            //Other Math functions
+            double squareRoot = Math.Sqrt(144); // 12
+            double power = Math.Pow(2, 3); // 8
+            double absoluteValue = Math.Abs(-15);
+
+        }
+    }
+}
---
+++ String Tricks/String Tricks/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace StringTricks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string FirstName = "Bromor";
+            string LastName = "Plays";
+            int age = 99;
+
+            //concatenation
+            string ConcatString = "Description: "+FirstName + " " + LastName + " " + age;
+
+            //Console.WriteLine(ConcatString);
+
+            //escaping characters(\)
+            string quote = "He said: \"something\"";//escape sequence for double quotes INtext
+            Console.WriteLine(quote);
+
+            string path = "C:\\SomeFolder\\SomeFile.txt"; //double backslash for  file path
+            Console.WriteLine(path);
+
+            string networkPath = "\\\\Computer\\Somefolder\\Somefile.txt";
+            Console.WriteLine(networkPath);
+
+            string newLine = "This is Line 1.\n\tThis is Line 2.\nThis is Line 3";  // \n for new line , \t for tab
+            Console.WriteLine(newLine);
+
+            //verbatim string literals
+            string literal = @"\\computer\someFolder\someFile.txt quote: ""asdasd"" "; //havent seen this one used except for file paths
+            Console.WriteLine(literal);
+
+            //string interpolation
+            string interpolated = $"Description: {FirstName} {LastName},{age}";
+            Console.WriteLine(interpolated);
+
+            
+
+
+        }
+    }
+}
